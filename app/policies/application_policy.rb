f9ce7116e54fb175dc2041205239e060973e4c78
Vadim-Kurnatovskiy <vadim.kurnatovskiy@flatstack.com>
2020-06-11 10:37:36 +0300

    Update policy config

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -1,5 +1,37 @@
 # frozen_string_literal: true
 
-class ApplicationPolicy < ActionPolicy::Base
+class ApplicationPolicy
+  include ActionPolicy::Policy::Core
+  include ActionPolicy::Policy::Authorization
+  include ActionPolicy::Policy::Cache
+
   authorize :user, allow_nil: true
+
+  def manage?
+    false
+  end
+
+  def index?
+    manage?
+  end
+
+  def new?
+    manage?
+  end
+
+  def edit?
+    manage?
+  end
+
+  def destroy?
+    manage?
+  end
+
+  def create?
+    new?
+  end
+
+  def update?
+    edit?
+  end
 end
