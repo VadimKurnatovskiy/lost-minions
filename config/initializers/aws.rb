b508a6c727344eaa83e0346ed4d7cbd28a08aab8
Vadim-Kurnatovskiy <vadim.kurnatovskiy@flatstack.com>
2020-06-14 22:45:13 +0300

    disable rubocop

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -2,9 +2,11 @@
 
 require 'aws-sdk-s3'
 
-Aws.config.update(
+# rubocop:disable Style/BracesAroundHashParameters, Layout/IndentFirstHashElement
+Aws.config.update({
   region: ENV.fetch('AWS_REGION', 'eu-central-1'),
-  credentials: Aws::Credentials.new(ENV.fetch('AWS_ACCESS_KEY_ID', 'id'), ENV.fetch('AWS_SECRET_ACCESS_KEY'), 'key')
-)
+  credentials: Aws::Credentials.new(ENV.fetch('AWS_ACCESS_KEY_ID', 'id'), ENV.fetch('AWS_SECRET_ACCESS_KEY', 'key'))
+})
+# rubocop:enable Style/BracesAroundHashParameters, Layout/IndentFirstHashElement
 
 S3_BUCKET = Aws::S3::Resource.new.bucket(ENV.fetch('AWS_BUCKET', 'bucket'))
