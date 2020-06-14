2e2c6ea83382efba1c2097046d6cfae7730ebc6e
Vadim-Kurnatovskiy <vadim.kurnatovskiy@flatstack.com>
2020-06-14 22:27:56 +0300

    use fetch

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -3,8 +3,8 @@
 require 'aws-sdk-s3'
 
 Aws.config.update(
-  region: ENV['AWS_REGION'],
-  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
+  region: ENV.fetch('AWS_REGION', 'eu-central-1'),
+  credentials: Aws::Credentials.new(ENV.fetch('AWS_ACCESS_KEY_ID', 'id'), ENV.fetch('AWS_SECRET_ACCESS_KEY'), 'key')
 )
 
-S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['AWS_BUCKET'])
+S3_BUCKET = Aws::S3::Resource.new.bucket(ENV.fetch('AWS_BUCKET', 'bucket'))
